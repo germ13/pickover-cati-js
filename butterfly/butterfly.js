65c5f4001e740e005ec036f871729cd079cc2b40
juan serrano <germgerm@gmail.com>
2019-06-09 19:03:15 -0700

    Fix only 3 colors displaying instead of 4 and refactor color selection.

--- butterfly/butterfly.js
+++ butterfly/butterfly.js
@@ -1,12 +1,14 @@
-const PI = Math.PI;
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
+
+const PI = Math.PI;
 let x = 0;
 let y = 0;
 
 var lastPoint = { x: canvas.width / 2, y: canvas.height / 2 };
+context.lineWidth = 0.5;
 
-for (let theta = 0; theta < 2 * PI; theta += 0.01) {
+for (let theta = 0; theta < 10 * PI; theta += 0.010) {
   r =
     Math.exp(Math.cos(theta)) -
     2 * Math.cos(4 * theta) +
@@ -15,31 +17,32 @@ for (let theta = 0; theta < 2 * PI; theta += 0.01) {
   x = r * Math.cos(theta);
   y = r * Math.sin(theta);
 
-  xx = x * 30 + canvas.width / 2;
-  yy = y * 30 + canvas.height / 2;
+  xx = x * 60 + canvas.width / 2;
+  yy = y * 60 + canvas.height / 2;
 
   context.beginPath();
   context.moveTo(lastPoint.x, lastPoint.y);
-  context.lineWidth = 1;
+  context.strokeStyle = setStrokeStyle(x, y);
   context.lineTo(xx, yy);
-  if (x > 0 && y > 0) {
-    context.strokeStyle = "#F00"; // red
-    // console.log(0);
-  } else if (x > 0 && y < 0) {
-    context.strokeStyle = "#F00";
-    // console.log(1);
-  } else if (x < 0 && y < 0) {
-    context.strokeStyle = "#00F"; // blue
-    // console.log(2);
-  } else if (x < 0 && y > 0) {
-    context.styleStyle = "#0AF"; // light blue
-    // console.log(3);
-  } else {
-    context.strokeStyle = "#000"; // black
-    // console.log("A");
-  }
-
   context.stroke();
   context.closePath();
-  (lastPoint.x = xx), (lastPoint.y = yy);
+  lastPoint.x = xx;
+  lastPoint.y = yy;
+}
+
+function setStrokeStyle(x, y) {
+  if (x > 0 && y > 0) {
+    return "red";
+  } else if (x > 0 && y < 0) {
+    return "green";
+  }
+  else if (x < 0 && y > 0) {
+    return "black";
+  }
+  else if (x < 0 && y < 0) {
+    return "blue";
+  }
+  else {
+    return "black"; // black
+  }
 }

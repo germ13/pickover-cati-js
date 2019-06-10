c83059b59a7f33333d8c05a2b3b8a8bd365f8a3d
juan serrano <germgerm@gmail.com>
2019-06-10 00:07:41 -0700

    Add two different color palettes

--- butterfly/butterfly.js
+++ butterfly/butterfly.js
@@ -1,12 +1,47 @@
-var canvas = document.getElementById("myCanvas");
-var context = canvas.getContext("2d");
+let canvas = document.getElementById("myCanvas");
+let context = canvas.getContext("2d");
+context.lineWidth = 0.5;
 
 const PI = Math.PI;
 let x = 0;
 let y = 0;
+let maxx = 0;
+let maxy = 0;
 
 var lastPoint = { x: canvas.width / 2, y: canvas.height / 2 };
-context.lineWidth = 0.5;
+
+let palette = {
+  radial : function (x, y) {
+    let distance = Math.sqrt(x * x + y * y);
+    if (distance < 1) {
+      return "red";
+    } else if (distance < 2) {
+      return "green";
+    } else if (distance < 3) {
+      return "blue";
+    } else if (distance < 4) {
+      return "yellow";
+    } else {
+      return "black";
+    }
+  },
+  quadrant : function (x, y) {
+    if (x > 0 && y > 0) {
+      return "red";
+    } else if (x > 0 && y < 0) {
+      return "green";
+    }
+    else if (x < 0 && y > 0) {
+      return "black";
+    }
+    else if (x < 0 && y < 0) {
+      return "blue";
+    }
+    else {
+      return "black"; // black
+    }
+  }
+}
 
 for (let theta = 0; theta < 10 * PI; theta += 0.010) {
   r =
@@ -17,12 +52,15 @@ for (let theta = 0; theta < 10 * PI; theta += 0.010) {
   x = r * Math.cos(theta);
   y = r * Math.sin(theta);
 
+  maxx = (Math.max(Math.abs(x) > maxx)) ? Math.max(Math.abs(x)) : maxx;
+  maxy = (Math.max(Math.abs(y) > maxy)) ? Math.max(Math.abs(y)) : maxy;
+
   xx = x * 60 + canvas.width / 2;
   yy = y * 60 + canvas.height / 2;
 
   context.beginPath();
   context.moveTo(lastPoint.x, lastPoint.y);
-  context.strokeStyle = setStrokeStyle(x, y);
+  context.strokeStyle = palette.radial(x, y);
   context.lineTo(xx, yy);
   context.stroke();
   context.closePath();
@@ -30,19 +68,4 @@ for (let theta = 0; theta < 10 * PI; theta += 0.010) {
   lastPoint.y = yy;
 }
 
-function setStrokeStyle(x, y) {
-  if (x > 0 && y > 0) {
-    return "red";
-  } else if (x > 0 && y < 0) {
-    return "green";
-  }
-  else if (x < 0 && y > 0) {
-    return "black";
-  }
-  else if (x < 0 && y < 0) {
-    return "blue";
-  }
-  else {
-    return "black"; // black
-  }
-}
+console.log("max", maxx, maxy);
